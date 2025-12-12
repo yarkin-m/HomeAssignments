e4c96cda13c29eb2c07876b65051329e79188ea4
yarkin-m <st141442@student.spbu.ru>
2025-12-12 16:06:34 +0300

    Add HomeAssignment2 with tasks a

---
+++ HomeAssignment2/a/reverse.h
@@ -0,0 +1,12 @@
+/*
+    Yarkin Makar
+    st141442@student.spbu.ru
+    Assignment 2a
+*/
+#ifndef REVERSE_H
+#define REVERSE_H
+#include <string>
+
+void reverseFile(const std::string& inputFile, const std::string& outputFile);
+
+#endif
