8a96081fc850a596ff41339b41d19028fd442e54
yarkin-m <st141442@student.spbu.ru>
2025-12-26 14:48:13 +0300

    Assignment4

--- HomeAssignment3/Autobot.cpp
+++ HomeAssignment3/Autobot.cpp
@@ -1,19 +1,18 @@
 /*
  *   Yarkin Makar
  *   st141442@student.spbu.ru
- *   Assignment 3
+ *   Assignment 4
  */
 #include "Autobot.h"
 #include <iostream>
 
 using namespace std;
 
-Autobot::Autobot(const std::string& name, int height, int weight, int power_level, 
-                 std::unique_ptr<Weapon> weapon, Alliance* alliance, 
-                 const std::string& vehicle_form, int courage_level):
-        Transformer(name, height, weight, power_level, std::move(weapon), alliance),
+Autobot::Autobot(const std::string& name, int height, int weight, int power_level, Weapon* weapon, Alliance* alliance, 
+    const std::string& vehicle_form, int courage_level):
+        Transformer(name, height, weight, power_level, weapon, alliance),
         vehicle_form_(vehicle_form), courage_level_(courage_level) {
-            cout << "Autobot " << name << " created" << endl;
+            cout << "Автобот " << name << " создан" << endl;
 }
 
 string Autobot::GetVehicleForm() const {
@@ -24,7 +23,7 @@ int Autobot::GetCourageLevel() const {
   return courage_level_;
 }
 
-//setters
+//setы
 void Autobot::SetVehicleForm(const std::string& vehicle_form) {
   vehicle_form_ = vehicle_form;
 }
@@ -33,11 +32,11 @@ void Autobot::SetCourageLevel(int courage_level) {
   courage_level_ = courage_level;
 }
 
-//additional methods
+//доп.
 string Autobot::ProtectHumans() {
-  return GetName() + " protects people with courage level " + to_string(courage_level_);
+  return GetName() + " защищает людей с уровнем храбрости " + to_string(courage_level_);
 }
 
 std::string Autobot::Transform() {
-  return GetName() + " transforms into " + vehicle_form_;
+  return GetName() + " трансформируется в " + vehicle_form_;
 }
