35f1eb17dacc173836e8b2c6b0cf0fc829f171d6
yarkin-m <st141442@student.spbu.ru>
2025-12-26 14:45:48 +0300

    Assignment4

--- HomeAssignment3/Alliance.h
+++ HomeAssignment3/Alliance.h
@@ -1,14 +1,14 @@
 /*
  *   Yarkin Makar
  *   st141442@student.spbu.ru
- *   Assignment 3
+ *   Assignment 4
  */
 #ifndef TRANSFORMERS_ALLIANCE_H
 #define TRANSFORMERS_ALLIANCE_H
 
 #include <string>
 
-// ассоциации
+// assotiation
 class Alliance {
  public:
   Alliance(const std::string& name, const std::string& leader);
@@ -17,7 +17,7 @@ class Alliance {
   std::string GetName() const;
   std::string GetLeader() const;
 
-  // Set методы
+  // Sets
   void SetName(const std::string& name);
   void SetLeader(const std::string& leader);
 
